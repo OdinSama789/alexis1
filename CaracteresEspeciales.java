9f06536ac7260d57e41755cad3c57f92327ae426
ODIN_SAMA <202213854k@utea.edu.pe>
2024-01-15 09:47:22 -0500

    primer commit

---
+++ CaracteresEspeciales.java
@@ -0,0 +1,6 @@
+public class CaracteresEspeciales {
+    public static void main(string[] args) {
+        system.out.println("Una corcheta: \u266A");
+        system.out.print("Dos corchetas: \u266B");
+    }
+}
